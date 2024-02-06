8276dd77b2cb14b6850ad5ffa7c1ceaca06a6e86
Vlad <s67.freelance@gmail.com>
2024-02-06 22:27:43 +0300

    Add StaticLesson20

---
+++ src/main/java/com/youtube/StaticLesson20.java
@@ -0,0 +1,41 @@
+package com.youtube;
+
+public class StaticLesson20 {
+    public static void main(String[] args) {
+        Human h1 = new Human("Боб", 40);
+        Human h2 = new Human("Vlad", 20);
+        h2.printAllCountHuman();
+    }
+
+    static class Human {
+        private static int countPeople;
+        private String name;
+        private int age;
+
+        public Human(String name, int age) {
+            this.name = name;
+            this.age = age;
+            countPeople++;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+
+        public void printAllCountHuman() {
+            System.out.println("Всего Человек: " + countPeople);
+        }
+    }
+}
