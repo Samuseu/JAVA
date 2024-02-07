c74e1c645a1c02d99a27d835a52e31a465d2d3fd
Vlad <s67.freelance@gmail.com>
2024-02-08 00:35:45 +0300

    Add Main class

---
+++ src/main/java/com/youtube/test/nasledie/Main.java
@@ -0,0 +1,11 @@
+package com.youtube.test.nasledie;
+
+public class Main {
+    public static void main(String[] args) {
+        Animal animal = new Animal();
+        Dog dog = new Dog("Bob",2);
+        dog.getInfo();
+        System.out.println(dog);
+
+    }
+}
